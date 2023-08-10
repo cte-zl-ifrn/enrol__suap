55ad74e37592c5228d8dde9dde641a254dbc68d8
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2023-08-10 15:11:42 -0300

    release: 1.0.001

--- version.php
+++ version.php
@@ -24,6 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$plugin->release = '1.0.001';
 $plugin->version   = 2022112800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022111800;        // Requires this Moodle version.
 $plugin->component = 'enrol_suap';    // Full name of the plugin (used for diagnostics)
